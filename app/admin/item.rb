2f74155377ff3fcb58e00344704637ead4bc326d
Raghu Betina <raghu@firstdraft.com>
2016-10-09 07:30:56 +0000

    Whitelist columns in Item for ActiveAdmin

--- app/admin/item.rb
+++ app/admin/item.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Item do
 
+ permit_params :bin_id, :image
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
